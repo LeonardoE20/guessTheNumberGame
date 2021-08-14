04d64189f813181902ff4f4b561b496f040abb30
Leonardo Escudero <leonardolearns@gmail.com>
2021-08-14 13:24:24 -0700

    Added another function called playGame() and this is where it is going to wait until someone enters a value or guesses the number and then shows the value in our console.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /**
  * Guess The Number Game
- * TODO: Get user value from input and save it to variable numberGuess
+ * Done: Get user value from input and save it to variable numberGuess
  * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
  * TODO: Console whether the guess is too high, too low, or is correct inside playGame function
  * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
@@ -19,4 +19,13 @@
 window.onload = function () {
   document.getElementById("number-submit").addEventListener("click", playGame);
   document.getElementById("restart-gmae").addEventListener("click", initGame);
+}
+
+/*
+functionality for playing the whole game
+This gets the user value from input(in HTML, it is an element) and save it to variable numberGuess
+*/
+function playGame() {
+  let numberGuess = document.getElementById("number-guess").value; // .value attribute allows us get the value of the input
+  console.log(numberGuess);
 }
